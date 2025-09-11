f7fb2edaaa4d355acaa4673dba906f6f3c35ca51
Taufiq Akbar <akbar.workcode@gmail.com>
2025-09-11 07:11:06 +0700

    modify test.py

--- testing/test.py
+++ testing/test.py
@@ -1,3 +1,4 @@
+import os
 import requests
 import re
 import time
@@ -17,7 +18,10 @@ def get_title_from_web(url, retries=10, delay=5):
             time.sleep(delay)
     return "Failed to get title after retries"
 
-web_url = "http://localhost:5002"
+host = os.environ.get("APP_HOST", "localhost")
+port = os.environ.get("APP_PORT", 5000)
+web_url = f"http://{host}:{port}"
+
 title = get_title_from_web(web_url)
 
 # Flexible assertion: just check if "Form Absensi" is in the title
